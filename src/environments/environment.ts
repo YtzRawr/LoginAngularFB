ffea3fa2b37b81434e2cc09f5380f91346210138
YtzRawr <matiaspizarro133@gmail.com>
2022-12-26 20:27:00 -0300

    add fire base

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'loginwithangular-449d1',
+    appId: '1:197782574458:web:33d4ea22bbc5947803b2c3',
+    storageBucket: 'loginwithangular-449d1.appspot.com',
+    apiKey: 'AIzaSyBwB23plfY4wudgsK2oAWvRdiu_JNMRH5M',
+    authDomain: 'loginwithangular-449d1.firebaseapp.com',
+    messagingSenderId: '197782574458',
+    measurementId: 'G-0L95TQQ1KQ',
+  },
+    production: false,
+  };
